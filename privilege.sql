084b223ddc3b08ecef0624c329b531c814af9700
LongMyDu <longmydu@gmail.com>
2021-10-30 23:13:13 +0700

    Phan quyen cho role admin

---
+++ privilege.sql
@@ -0,0 +1,14 @@
+﻿-- Tạo tài khoản mẫu để test quyền
+--exec sp_addlogin 'ExampleAdmin', '123456', 'DB_QLDatChuyenHang'
+--exec sp_addlogin 'LMD', '123456', 'DB_QLDatChuyenHang'
+
+--Create user ExampleAdmin For login ExampleAdmin
+--Create user LMD For login LMD 
+--exec sp_addrolemember 'Admin','ExampleAdmin'  -- Chỉ có exampleAdmin là thuộc role admin, LMD thì không
+
+-- Phân quyền cho role Admin
+exec sp_addrole 'Admin'
+GRANT SELECT, INSERT, UPDATE, DELETE ON NhanVien TO Admin WITH GRANT OPTION
+GRANT SELECT, INSERT, UPDATE, DELETE ON [Admin] TO Admin WITH GRANT OPTION
+GRANT SELECT, UPDATE ON TaiKhoan TO Admin WITH GRANT OPTION 
+

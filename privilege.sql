90d2eb1aedb792b06b1f3568378f7e8a72323551
tktruc211 <tktruc19@clc.fitus.edu.vn>
2021-10-31 20:05:53 +0700

    Update privilege.sql
    phân quyền Đối tác

--- privilege.sql
+++ privilege.sql
@@ -6,9 +6,18 @@
 --Create user LMD For login LMD 
 --exec sp_addrolemember 'Admin','ExampleAdmin'  -- Chỉ có exampleAdmin là thuộc role admin, LMD thì không
 
+use DB_QLDatChuyenHang
+go
+
 -- Phân quyền cho role Admin
 exec sp_addrole 'Admin'
 GRANT SELECT, INSERT, UPDATE, DELETE ON NhanVien TO Admin WITH GRANT OPTION
 GRANT SELECT, INSERT, UPDATE, DELETE ON [Admin] TO Admin WITH GRANT OPTION
 GRANT SELECT, UPDATE ON TaiKhoan TO Admin WITH GRANT OPTION 
 
+-- Phân quyền cho role Đối tác
+exec sp_addrole 'DoiTac'
+GRANT SELECT ON [DONHANG] TO [DOITAC] WITH GRANT OPTION
+GRANT UPDATE ON [DONHANG]([TinhTrangVanChuyen]) TO [DOITAC] WITH GRANT OPTION
+GRANT SELECT ON [HOPDONG] TO [DOITAC] WITH GRANT OPTION
+GRANT SELECT, INSERT, UPDATE, DELETE ON [SANPHAM] TO [DOITAC] WITH GRANT OPTION

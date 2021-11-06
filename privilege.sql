78b2d3c21c4681147fd66ef8b28097135b9f897f
LongMyDu <longmydu@gmail.com>
2021-11-07 00:00:33 +0700

    Cấp quyền exec cho admin

--- privilege.sql
+++ privilege.sql
@@ -7,9 +7,12 @@ ADMIN
 exec sp_addrole 'Admin'
 GRANT SELECT, INSERT, UPDATE, DELETE ON NhanVien TO Admin WITH GRANT OPTION
 GRANT SELECT, INSERT, UPDATE, DELETE ON [Admin] TO Admin WITH GRANT OPTION
-GRANT SELECT, INSERT, UPDATE, DELETE ON TaiKhoan TO Admin WITH GRANT OPTION 
+GRANT SELECT, INSERT, UPDATE, DELETE ON TaiKhoan TO Admin WITH GRANT OPTION
+USE DB_QLDatChuyenHang
+GRANT EXECUTE ON DATABASE::DB_QLDatChuyenHang To Admin 
+  
 -- Tạo tài khoản login và user cho Admin
-exec sp_addlogin 'lg_admin', 'admin123'
+exec sp_addlogin 'lg_admin', 'admin123', 'DB_QLDatChuyenHang'
 Create user us_admin For login lg_admin
 exec sp_addrolemember 'Admin','us_admin'
 
@@ -36,7 +39,7 @@ grant select, insert, delete on Chitietdonhang to KhachHang
 grant update on chitietdonhang(Masp, soluong) to KhachHang
 
 -- Tạo tài khoản login và user cho khách hàng
-exec sp_addlogin 'lg_khachhang', 'khachhang123'
+exec sp_addlogin 'lg_khachhang', 'khachhang123', 'DB_QLDatChuyenHang'
 Create user us_khachhang For login lg_khachhang
 exec sp_addrolemember 'KhachHang','us_khachhang'
 
@@ -50,7 +53,7 @@ grant select on donhang to taixe
 grant update on donhang([tinhtrangvanchuyen]) to taixe
 grant select on khachhang to taixe
 -- Tạo tài khoản login và user cho tài xế
-exec sp_addlogin 'lg_taixe', 'taixe123'
+exec sp_addlogin 'lg_taixe', 'taixe123', 'DB_QLDatChuyenHang'
 Create user us_taixe For login lg_taixe
 exec sp_addrolemember 'Taixe','us_taixe'
 
@@ -62,7 +65,7 @@ exec sp_addrole 'Nhanvien'
 grant select on Hopdong to Nhanvien 
 grant update on Hopdong([PhiHoaHong],[thoigianhieuluc],[tinhtrangduyet]) to Nhanvien
 -- Tạo tài khoản login và user cho nhân viên
-exec sp_addlogin 'lg_nhanvien', 'nhanvien123'
+exec sp_addlogin 'lg_nhanvien', 'nhanvien123', 'DB_QLDatChuyenHang'
 Create user us_nhanvien For login lg_nhanvien
 exec sp_addrolemember 'Nhanvien','us_nhanvien'
 
@@ -91,7 +94,7 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON [SANPHAM] TO [DOITAC]
 GRANT SELECT, UPDATE ON [DOITAC] TO [DOITAC]
 
 -- Tạo tài khoản login và user cho đối tác
-exec sp_addlogin 'lg_doitac', 'doitac123'
+exec sp_addlogin 'lg_doitac', 'doitac123', 'DB_QLDatChuyenHang'
 Create user us_doitac For login lg_doitac
 exec sp_addrolemember 'DoiTac','us_doitac'
 

292b2de983b885990c0cebc445d2fde9f094927e
tktruc211 <tktruc19@clc.fitus.edu.vn>
2021-11-21 20:17:45 +0700

    Dirty Read

--- 19127304/Tran_1_DirtyRead.sql
+++ 19127304/Tran_1_DirtyRead.sql
@@ -1,50 +1,9 @@
 ﻿-- Dirty Read
--- Xem số lượng tồn của sản phẩm '100028'
--- Transaction 1: Cập nhật số lượng tồn của sản phẩm, sau đó hủy
+-- Transaction 1: Bán 8 sản phẩm '100028', sau đó bị hủy do lỗi số lượng
 
 USE DB_QLDatChuyenHang
 GO
 
-Begin Tran T1
-	Declare @masp int, @soluongton int, @soluongban int
-	Select @masp = 100028
-
-	if not exists
-	(
-		select *
-		from SANPHAM SP
-		where SP.MaSP = @masp
-	)
-	Begin
-		Raiserror('Không tồn tại sản phẩm này.', 16, 1)
-		Rollback tran
-	End
-
-	else
-	Begin
-		Set @soluongban = 8
-		Select @soluongton =(select SoLuongTon
-							from SANPHAM where MaSP = @masp)
-
-		Set @soluongton = @soluongton - @soluongban
-
-		Update SANPHAM
-		Set SoLuongTon = @soluongton
-		Where MaSP = @masp
-
-		Waitfor DELAY '00:00:10'
-		if exists
-		(
-			select *
-			from SANPHAM SP
-			where SP.MaSP = @masp and SP.SoLuongTon < 0
-		)
-		Begin
-			Raiserror('Sản phẩm không đủ số lượng.', 16, 1)
-			Rollback Tran T1
-		End
-
-	End
-Commit Tran T1
+exec dbo.Ban_SanPham_SoLuong '100028', '8';
 
 Select * from SANPHAM
--- 19127304/Tran_2_DirtyRead.sql
+++ 19127304/Tran_2_DirtyRead.sql
@@ -1,10 +1,7 @@
 ﻿-- Dirty Read
--- Xem số lượng tồn của sản phẩm '100001'
--- Transaction 2: Xem thông tin sản phẩm
+-- Transaction 2: Xem thông tin sản phẩm '100028' thuộc chi nhánh '100023'
 
 USE DB_QLDatChuyenHang
 GO
 
-Set transaction isolation level read uncommitted
-
-Select * from SANPHAM
+exec dbo.XemTatCa_SANPHAM_ThuocChiNhanh '100023';

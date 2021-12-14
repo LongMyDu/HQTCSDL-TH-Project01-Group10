462e9796fa60f984a40dfa66acf6d9b65b453371
LongMyDu <longmydu@gmail.com>
2021-12-14 21:30:18 +0700

    Xong: gửi thông tin đơn đặt hàng tới server

--- UI/server.js
+++ UI/server.js
@@ -31,11 +31,14 @@ app.post('/insert-order-post', async function (req, res) {
       tongtien: req.body.TongTien,
       phiVC: req.body.PhiVC,
       chinhanh: req.body.MaChiNhanh,
-      product_detail_list: JSON.parse(req.body.product_detail_list)
+      cart_list: JSON.parse(req.body.cart_list)
    };
     
    //TODO: Thêm đơn hàng mới vào DB
+   console.log("Đơn hàng được gửi tới: ", response);
 
+   //TODO: Gửi thông báo lại cho client:
+   res.send("Insert successfully!");
 })
 
 
@@ -51,14 +54,11 @@ app.get('/api/sanpham-list', function (req, res) {
         return;
       }
       var totalItems = result.recordset.length;
-      
       const total_sanpham_list = result.recordset.map(elm => ({ id: elm.MaSP, tenSP: elm.TenSP, gia: elm.Gia, soLuongTon: elm.SoLuongTon, chiNhanh: elm.MaChiNhanh}));
       
       // Send to res
       res.json({totalItems: totalItems, sanpham_list: total_sanpham_list});
    });
-
-   console.log("hello");
 })
 
 
@@ -143,3 +143,85 @@ sql.connect(config, err => {
 //     }).catch(err => {
 //     });
 //  }
+
+
+
+// app.post('/insert-receipt-post', async function (req, res) {
+//    // Prepare output in JSON format
+//    let response = {
+//       customer_id:req.body.customer_id,
+//       date: req.body.date,                      //yyyy-mm-dd
+//       product_detail_list: JSON.parse(req.body.product_detail_list)
+//    };
+   
+//    //TODO: validate response
+//    //let isValid = true;
+
+//    // Get customer ID and date
+//    var customer_id = response.customer_id;
+//    var date = response.date;
+
+//    // Get all products
+//    var product_detail_list = response.product_detail_list;
+
+//    // Case 1: Insufficient information
+//    if (customer_id === '' || date === '' || !NoneEmpty(product_detail_list)) {
+//       res.send("Cannot insert! Some information is missing!");
+//       return;
+//    } 
+
+//    // Case 2: Wrong date (inputDate > today)
+//    // Note: Wrong date format has already been checked via the front-end
+//    var inputDate = new Date(date);
+//    var today = new Date();
+//    if (inputDate.setHours(0,0,0,0) > today.setHours(0,0,0,0)) {
+//       res.send("Cannot insert! Invalid date!");
+//       return;
+//    }
+
+//    // Case 3: Wrong customer_id
+//    let isValid = await checkCustomerID(customer_id);
+//    if (isValid !== true) {
+//       res.send(`Error from checking customer id: ${customer_id}`);
+//       return;
+//    }
+//    // Case 4: Wrong product_id
+//    for (let i = 0; i < product_detail_list.length; i++) {
+//       isValid = await checkProductID(product_detail_list[i]);
+//       if (isValid !== true) {
+//          res.send(`Error from checking product id: ${product_detail_list[i].product_id}`);
+//          return;
+//       } 
+//    }
+
+//    // Case 5: Duplicate product_id
+//    for (let i = 0; i < product_detail_list.length - 1; i++) {
+//       for (let j = i+1; j < product_detail_list.length; j++) {
+//          if (product_detail_list[i].product_id === product_detail_list[j].product_id) {
+//             res.send(`Duplicate product id: ${product_detail_list[i].product_id}`);
+//             return;
+//          }
+//       }
+//    }
+
+//    //TODO: link to DB and insert new row into table HoaDon and CT_HoaDon
+//    new_receipt_id = {}
+//    isValid = await generateReceiptID(new_receipt_id);
+//    if (isValid !== true) {
+//       res.send("Database is full!");
+//       return;
+//    }
+
+//    new_receipt_id.value = ('0'.repeat(6 - new_receipt_id.value.length)) + new_receipt_id.value;
+//    isValid = await insertIntoHoaDon(new_receipt_id, customer_id, date);
+//    if (isValid !== true) {
+//       res.send("Connection failed when inserting your receipt!");
+//       return;
+//    }
+//    isValid = await insertIntoCT_HoaDon(new_receipt_id, product_detail_list);
+//    if (isValid !== true) {
+//       res.send(`Connection failed when inserting into CT_HoaDon!`);
+//       return;
+//    }
+//    res.send("Insert successfully!");
+// })

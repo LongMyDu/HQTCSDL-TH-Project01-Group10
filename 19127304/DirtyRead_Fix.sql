c41ad241d29353619ff89e0b9830ccc24f271a01
tktruc211 <tktruc19@clc.fitus.edu.vn>
2021-12-18 14:45:36 +0700

    Sửa lỗi tranh chấp

---
+++ 19127304/DirtyRead_Fix.sql
@@ -0,0 +1,76 @@
+﻿USE DB_QLDatChuyenHang
+GO
+
+-- Transaction 1: Bán 8 sản phẩm '100028', sau đó hủy do lỗi số lượng
+alter procedure Ban_SanPham_SoLuong
+(
+	@masp int,
+	@soluongban int
+)
+as
+Begin Tran
+	Declare @soluongton int
+
+	if not exists
+	(
+		select *
+		from SANPHAM SP
+		where SP.MaSP = @masp
+	)
+	Begin
+		Raiserror('Không tồn tại sản phẩm này.', 16, 1)
+		Rollback tran
+	End
+
+	else
+	Begin
+		Set @soluongton =(select SoLuongTon
+							from SANPHAM where MaSP = @masp)
+
+		Set @soluongton = @soluongton - @soluongban
+
+		Update SANPHAM
+		Set SoLuongTon = @soluongton
+		Where MaSP = @masp
+
+		Waitfor DELAY '00:00:05'
+		if exists
+		(
+			select *
+			from SANPHAM SP
+			where SP.MaSP = @masp and SP.SoLuongTon < 0
+		)
+		Begin
+			Raiserror('Sản phẩm không đủ số lượng.', 16, 1)
+			Rollback Tran
+		End
+
+	End
+Commit Tran
+Go
+
+-- Transaction 2: Xem thông tin sản phẩm '100028' thuộc chi nhánh '100023'
+alter procedure XemTatCa_SANPHAM_ThuocChiNhanh
+(
+	@MaChiNhanh int
+)
+as
+begin tran
+	if @MaChiNhanh != NULL and not exists 
+	(
+		select *
+		from CHINHANH CN
+		where CN.MaChiNhanh = @MaChiNhanh
+	)
+	begin
+		raiserror('Không tìm thấy chi nhánh.', 16, 1);
+		rollback tran
+	end
+	else 
+	begin
+		Select *
+		From SANPHAM
+		Where MaChiNhanh = @MaChiNhanh
+		commit tran
+	end
+GO
